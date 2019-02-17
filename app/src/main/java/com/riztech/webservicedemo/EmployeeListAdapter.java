b45756eb3308cf980a6fdda89fc447b4b071eea0
Rizwanul Haque <riztech.dev@gmail.com>
2019-02-17 12:31:30 +0530

    Added recyclerview

---
+++ app/src/main/java/com/riztech/webservicedemo/EmployeeListAdapter.java
@@ -0,0 +1,59 @@
+package com.riztech.webservicedemo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.riztech.webservicedemo.models.Employee;
+
+import java.util.List;
+
+public class EmployeeListAdapter extends RecyclerView.Adapter<EmployeeListAdapter.EmployeeViewHolder> {
+
+    List<Employee> employeeList;
+
+    public EmployeeListAdapter(List<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
+
+    @NonNull
+    @Override
+    public EmployeeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_employee_list, parent, false);
+
+        return new EmployeeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EmployeeViewHolder holder, int position) {
+        Employee employee = employeeList.get(position);
+        holder.txtPhoneNumber.setText(employee.getPhoneNumber());
+        holder.txtName.setText(employee.getName());
+        holder.txtAddress.setText(employee.getAddress());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return employeeList.size();
+    }
+
+    class EmployeeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtName, txtAddress, txtPhoneNumber;
+        ImageView imgDelete;
+
+        public EmployeeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtAddress = itemView.findViewById(R.id.txtAddress);
+            txtPhoneNumber = itemView.findViewById(R.id.txtPhoneNumber);
+            imgDelete = itemView.findViewById(R.id.imgDelete);
+        }
+    }
+}
